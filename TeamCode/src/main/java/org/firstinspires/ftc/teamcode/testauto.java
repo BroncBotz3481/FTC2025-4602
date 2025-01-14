43db6a49583fbd36f7cbca5b11809090ba4cc8c2
aayyden <w.aidenhuffman@gmail.com>
2025-01-14 17:59:45 -0600

    Auto with specimin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testauto.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous (name="Sanjith is him 2.0", group="4602")
+@Autonomous (name="Specimen Auto", group="4602")
 public class testauto extends LinearOpMode {
 
     Team4602HM2025 robot = new Team4602HM2025();
@@ -15,10 +15,6 @@ public class testauto extends LinearOpMode {
     @Override
     public void runOpMode() {
         robot.Map(hardwareMap);
-        robot.Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.Arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.Arm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         moveForward(0.4, 3000);
         //write code here
@@ -27,48 +23,69 @@ public class testauto extends LinearOpMode {
     }
 
     public void moveForward(double power, int time) {
-        robot.Arm.setTargetPosition(500);
-        robot.Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.Arm.setPower(0.8);
-        robot.Arm2.setTargetPosition(500);
-        robot.Arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.Arm2.setPower(0.8);
-        sleep(2000);
+        robot.Intake.setPosition(1);
+        sleep(5000);
+
+
+//        robot.DriveRightFront.setPower(0.8); //Frontleft & backright go the same way others go opposite for strafing
+//        robot.DriveLeftFront.setPower(-0.6);
+//        robot.DriveRightBack.setPower(-0.8);
+//        robot.DriveLeftBack.setPower(-0.6);
+//        sleep(1);                //strafe
+//        robot.DriveRightFront.setPower(0);
+//        robot.DriveLeftFront.setPower(0);
+//        robot.DriveRightBack.setPower(0);
+//        robot.DriveLeftBack.setPower(0);
+
+        robot.Arm.setPower(0.6);
+        robot.Arm2.setPower(0.6);
+        sleep(1000);               //Arm Up
+        robot.Arm.setPower(0);
+        robot.Arm2.setPower(0);
+
+        sleep(1000);
         robot.DriveRightFront.setPower(0.8);
         robot.DriveLeftFront.setPower(0.6);
         robot.DriveRightBack.setPower(0.8);
         robot.DriveLeftBack.setPower(-0.6);
-        sleep(1500);
+        sleep(1000);                      //Move forward
         robot.DriveRightFront.setPower(0);
         robot.DriveLeftFront.setPower(0);
         robot.DriveRightBack.setPower(0);
         robot.DriveLeftBack.setPower(0);
 
         sleep(1000);
-        robot.DriveRightFront.setPower(0.8);
+
+        robot.Arm.setPower(-0.6);
+        robot.Arm2.setPower(-0.6);
+        sleep(500);               //arm downb
+        robot.Arm.setPower(0);
+        robot.Arm2.setPower(0);
+
+        sleep(1000);
+
+        robot.Intake.setPosition(0);
+        sleep(1000);
+        robot.DriveRightFront.setPower(-0.8);
         robot.DriveLeftFront.setPower(-0.6);
-        robot.DriveRightBack.setPower(0.8);
+        robot.DriveRightBack.setPower(-0.8);
         robot.DriveLeftBack.setPower(0.6);
-        sleep(400);
+        sleep(1000);                      //Move backward
         robot.DriveRightFront.setPower(0);
         robot.DriveLeftFront.setPower(0);
         robot.DriveRightBack.setPower(0);
         robot.DriveLeftBack.setPower(0);
 
-
-        robot.Intakemover.setPower(-1.0);
-        sleep(1000);
-        robot.Intakemover.setPower(0);
-        robot.take.setPower(-1.0);
-        sleep(3000);
-        robot.take.setPower(0);
-
-        robot.Arm.setTargetPosition(0);
-        robot.Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.Arm.setPower(0.8);
-        robot.Arm2.setTargetPosition(0);
-        robot.Arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.Arm2.setPower(0.8);
+        sleep(500);
+        robot.DriveRightFront.setPower(-0.8); //Frontleft & backright go the same way others go opposite for strafing
+        robot.DriveLeftFront.setPower(0.8);
+        robot.DriveRightBack.setPower(0.8);
+        robot.DriveLeftBack.setPower(0.8);
+        sleep(750);                //strafe
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+      robot.DriveLeftBack.setPower(0);
 
     }
 }
